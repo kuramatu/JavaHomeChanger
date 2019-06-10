1e906338c235e613203fb848ed1b7bb78ac40ad1
KuramatuHajime <30949451+kuramatu@users.noreply.github.com>
2019-06-10 17:04:45 +0900

    [update] It corresponds to the display of AdoptOpenJDK.

--- bat/jhc.bat
+++ bat/jhc.bat
@@ -13,11 +13,11 @@
 @rem "SearchTargetDirectories.conf" file lacated "JHC_HOME\conf\".
 @rem 
 @rem Usage: jhc [option]
-@rem 	jhc				Change "JAVA_HOME" to the selected JDK.
+@rem   jhc             Change "JAVA_HOME" to the selected JDK.
 @rem 
-@rem 	jhc update		Update JDK list and after update JavaHomeChanger run.
-@rem 					Please use it when new JDK is installed or
-@rem 					existing JDK is uninstalled.
+@rem   jhc update      Update JDK list and after update JavaHomeChanger run.
+@rem                   Please use it when new JDK is installed or
+@rem                   existing JDK is uninstalled.
 @rem 
 @rem ------------------------------------------------------------------
 
@@ -30,36 +30,35 @@ set JHC_COMMAND_LINE_ARG=%1
 set ERROR_CODE=0
 call :isAdmin
 if "!ERROR_CODE!"=="0" (
-	call :updateJDKList
-	call :existsJDKList
-	call :showJAVA_HOME
-	call :selectJDKVersion
-	call :setJAVA_HOME
+  call :updateJDKList
+  call :existsJDKList
+  call :showJAVA_HOME
+  call :selectJDKVersion
+  call :setJAVA_HOME
 )
 call :printEndStatus
 exit /b !ERROR_CODE!
 
 
-rem Check running this tool with administrator authority.
 :isAdmin
 openfiles > nul 2>&1
 if not "%ERRORLEVEL%"=="0" (
-	echo Please running this tool with administrator authority.
-	call :error
+  echo Please running this tool with administrator authority.
+  call :error
 )
 exit /b
 
 
 :updateJDKList
 if "%JHC_COMMAND_LINE_ARG%"=="update" (
-	call :makeJDKList
+  call :makeJDKList
 )
 exit /b
 
 
 :existsJDKList
 if not exist %JHC_HOME%conf\JDKList.conf (
-	call :makeJDKList
+  call :makeJDKList
 )
 exit /b
 
@@ -91,12 +90,12 @@ exit /b
 
 :viewJDKOptions
 if exist %JHC_HOME%conf\JDKList.conf (
-	for /f "tokens=1,2,3,4 delims=," %%a in (%JHC_HOME%conf\JDKList.conf) do (
-		set JAVA_DIRECTORY_PATH=%%d
-		call :isSettedJAVA_HOME
-		echo ^|%%a	^|!SETTED_MARK!	^|%%b	^|%%c
-	)
-	call :drawHorizon
+  for /f "tokens=1,2,3,4 delims=," %%a in (%JHC_HOME%conf\JDKList.conf) do (
+    set JAVA_DIRECTORY_PATH=%%d
+    call :isSettedJAVA_HOME
+    echo ^|%%a	^|!SETTED_MARK!	^|%%b	^|%%c
+  )
+  call :drawHorizon
 )
 exit /b
 
@@ -115,32 +114,31 @@ exit /b
 
 :isSettedJAVA_HOME
 if "%JAVA_HOME%"=="!JAVA_DIRECTORY_PATH!" (
-	set SETTED_MARK=***
+  set SETTED_MARK=***
 ) else (
-	set SETTED_MARK=
+  set SETTED_MARK=
 )
 exit /b
 
 
-rem Set system-environment-variable "JAVA_HOME"
 :setJAVA_HOME
 set SELECT_NOT_CHANGE_OPTION=false
 set IS_CHANGED=false
 if "!SELECT_KEY!"=="0" (
-	set SELECT_NOT_CHANGE_OPTION=true
-	exit /b
+  set SELECT_NOT_CHANGE_OPTION=true
+  exit /b
 )
 set /a COUNT_ROW=1
 for /f "tokens=4 delims=," %%a in (%JHC_HOME%conf\JDKList.conf) do (
-	if "!COUNT_ROW!"=="!SELECT_KEY!" (
-		setx /m JAVA_HOME "%%a" 2>&1 > nul
-		set IS_CHANGED=true
-	)
-	set /a COUNT_ROW+=1
+  if "!COUNT_ROW!"=="!SELECT_KEY!" (
+    setx /m JAVA_HOME "%%a" 2>&1 > nul
+    set IS_CHANGED=true
+  )
+  set /a COUNT_ROW+=1
 )
 if "!IS_CHANGED!"=="false" (
-	echo The inputted numbers did not match key numbers.
-	call :error
+  echo The inputted numbers did not match key numbers.
+  call :error
 )
 exit /b
 
@@ -152,14 +150,14 @@ exit /b
 
 :printEndStatus
 if "!SELECT_NOT_CHANGE_OPTION!"=="true" (
-	echo;
-	echo JAVA_HOME does not change.
+  echo;
+  echo JAVA_HOME does not change.
 ) else if "!ERROR_CODE!"=="1" (
-	echo;
-	echo Fail to change.
+  echo;
+  echo Fail to change.
 ) else (
-	echo;
-	echo Successfully changed.
+  echo;
+  echo Successfully changed.
 )
 exit /b
 
@@ -169,14 +167,14 @@ call :makeSearchTargetDirectoriesConfFile
 call :deleteJDKList
 set /a PRIMARY_KEY=1
 for /f "delims=" %%a in (%JHC_HOME%conf\SearchTargetDirectories.conf) do (
-	set SEARCH_TARGET_DIRECTORY=%%a
-	dir /a:d /b /o:n "!SEARCH_TARGET_DIRECTORY!" 2>nul | findstr /v "jre" > %JHC_HOME%conf\collect_jdk_directories.tmp 2> nul
-	for /f "delims=" %%b in (%JHC_HOME%conf\collect_jdk_directories.tmp) do (
-		set JDK_DIRECTORY=%%b
-		call :collectJDKInfo
-		call :addJDKList
-		set /a PRIMARY_KEY+=1
-	)
+  set SEARCH_TARGET_DIRECTORY=%%a
+  dir /a:d /b /o:n "!SEARCH_TARGET_DIRECTORY!" 2>nul | findstr /v "jre" > %JHC_HOME%conf\collect_jdk_directories.tmp 2> nul
+  for /f "delims=" %%b in (%JHC_HOME%conf\collect_jdk_directories.tmp) do (
+    set JDK_DIRECTORY=%%b
+    call :collectJDKInfo
+    call :addJDKList
+    set /a PRIMARY_KEY+=1
+  )
 )
 del %JHC_HOME%conf\*.tmp
 exit /b
@@ -184,18 +182,18 @@ exit /b
 
 :makeSearchTargetDirectoriesConfFile
 if not exist %JHC_HOME%conf (
-	mkdir %JHC_HOME%conf
+  mkdir %JHC_HOME%conf
 )
 if not exist %JHC_HOME%conf\SearchTargetDirectories.conf (
-	echo C:\Program Files^\Java^\>> %JHC_HOME%conf\SearchTargetDirectories.conf
-	echo C:\Program Files ^(x86^)^\Java^\>> %JHC_HOME%conf\SearchTargetDirectories.conf
+  echo C:\Program Files^\Java^\>> %JHC_HOME%conf\SearchTargetDirectories.conf
+  echo C:\Program Files ^(x86^)^\Java^\>> %JHC_HOME%conf\SearchTargetDirectories.conf
 )
 exit /b
 
 
 :deleteJDKList
 if exist %JHC_HOME%conf\JDKList.conf (
-	del %JHC_HOME%conf\JDKList.conf
+  del %JHC_HOME%conf\JDKList.conf
 )
 exit /b
 
@@ -208,29 +206,42 @@ exit /b
 
 
 :checkJDKVersion
-find "openjdk" %JHC_HOME%conf\java_version.tmp > nul
+set JDK_TYPE=OracleJDK
+call :isOpenJDK
 if !ERRORLEVEL! equ 0 (
-	set JDK_TYPE=OpenJDK
-) else (
-	set JDK_TYPE=OracleJDK
+  set JDK_TYPE=OpenJDK
+)
+call :isAdoptOpenJDK
+if !ERRORLEVEL! equ 0 (
+  set JDK_TYPE=AdoptOpenJDK
 )
 set /a JAVA_VERSION_TMP_FILE_ROW_COUNT=1
 for /f "tokens=3" %%a in (%JHC_HOME%conf\java_version.tmp) do (
-	set VERSION_NUMBER=%%a
-	if !JAVA_VERSION_TMP_FILE_ROW_COUNT!==1 (
-		set JDK_VERSION=!JDK_TYPE!-!VERSION_NUMBER:~1,-1!
-	)
-	set /a JAVA_VERSION_TMP_FILE_ROW_COUNT+=1
+  set VERSION_NUMBER=%%a
+  if !JAVA_VERSION_TMP_FILE_ROW_COUNT!==1 (
+    set JDK_VERSION=!JDK_TYPE!-!VERSION_NUMBER:~1,-1!
+  )
+  set /a JAVA_VERSION_TMP_FILE_ROW_COUNT+=1
 )
+exit /b
+
+
+:isOpenJDK
+find "openjdk" %JHC_HOME%conf\java_version.tmp > nul
+exit /b
+
+
+:isAdoptOpenJDK
+find "AdoptOpenJDK" %JHC_HOME%conf\java_version.tmp > nul
 exit /b
 
 
 :checkJavaBit
 find "64-Bit" %JHC_HOME%conf\java_version.tmp > nul
 if !ERRORLEVEL! equ 0 (
-	set JAVA_BIT=64bit
+  set JAVA_BIT=64bit
 ) else (
-	set JAVA_BIT=32bit
+  set JAVA_BIT=32bit
 )
 exit /b
 
